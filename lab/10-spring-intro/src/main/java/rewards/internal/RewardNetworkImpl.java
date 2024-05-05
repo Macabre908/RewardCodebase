b9ad329c0ab52c0b8e7a4a8760fdcfca0c8c000f
Abhishek Dubey <abhishekdubey789901@gmail.com>
2024-05-05 11:34:51 +0530

    Some additional functionalities

--- lab/10-spring-intro/src/main/java/rewards/internal/RewardNetworkImpl.java
+++ lab/10-spring-intro/src/main/java/rewards/internal/RewardNetworkImpl.java
@@ -11,27 +11,6 @@ import rewards.internal.restaurant.Restaurant;
 import rewards.internal.restaurant.RestaurantRepository;
 import rewards.internal.reward.RewardRepository;
 
-/**
- * Rewards an Account for Dining at a Restaurant.
- * 
- * The sole Reward Network implementation. This object is an application-layer service responsible for coordinating with
- * the domain-layer to carry out the process of rewarding benefits to accounts for dining.
- * 
- * Said in other words, this class implements the "reward account for dining" use case.
- *
- * TODO-00: In this lab, you are going to exercise the following:
- * - Understanding internal operations that need to be performed to implement
- *   "rewardAccountFor" method of the "RewardNetworkImpl" class
- * - Writing test code using stub implementations of dependencies
- * - Writing both target code and test code without using Spring framework
- *
- * TODO-01: Review the Rewards Application document (Refer to the lab document)
- * TODO-02: Review project dependencies (Refer to the lab document)
- * TODO-03: Review Rewards Commons project (Refer to the lab document)
- * TODO-04: Review RewardNetwork interface and RewardNetworkImpl class below
- * TODO-05: Review the RewardNetworkImpl configuration logic (Refer to the lab document)
- * TODO-06: Review sequence diagram (Refer to the lab document)
- */
 public class RewardNetworkImpl implements RewardNetwork {
 
 	private AccountRepository accountRepository;
@@ -54,14 +33,11 @@ public class RewardNetworkImpl implements RewardNetwork {
 	}
 
 	public RewardConfirmation rewardAccountFor(Dining dining) {
-		// TODO-07: Write code here for rewarding an account according to
-		//          the sequence diagram in the lab document
 		Account account = this.accountRepository.findByCreditCard(dining.getCreditCardNumber());
 		Restaurant restaurant = this.restaurantRepository.findByMerchantNumber(dining.getMerchantNumber());
 		MonetaryAmount benefit = restaurant.calculateBenefitFor(account, dining);
 		AccountContribution contribution = account.makeContribution(benefit);
 		this.accountRepository.updateBeneficiaries(account);
-        // TODO-08: Return the corresponding reward confirmation
 		return this.rewardRepository.confirmReward(contribution, dining);
 	}
 }
