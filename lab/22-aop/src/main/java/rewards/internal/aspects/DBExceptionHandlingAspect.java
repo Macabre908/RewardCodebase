60c5612ac9be1cfeb88856df89a1bf22439fab2b
Abhishek Dubey <abhishekdubey789901@gmail.com>
2024-05-05 11:37:06 +0530

    Some additional functionalities

--- lab/22-aop/src/main/java/rewards/internal/aspects/DBExceptionHandlingAspect.java
+++ lab/22-aop/src/main/java/rewards/internal/aspects/DBExceptionHandlingAspect.java
@@ -17,21 +17,10 @@ public class DBExceptionHandlingAspect {
 	
 	private Logger logger = LoggerFactory.getLogger(getClass());
 
-
-	//	TODO-10 (Optional): Use AOP to log an exception.
-	//  (Steps 10, 11 and 12 are optional, skip them if you are short on time)
-	//
-	//  - Configure this advice method to enable logging of
-	//	  exceptions thrown by Repository class methods.
-	//	- Select the advice type that seems most appropriate.
-
 	@AfterThrowing(value = "execution(* rewards.internal.*.*Repository.*(..))", throwing = "e")
 	public void implExceptionHandling(RewardDataAccessException e) {
 		// Log a failure warning
 		logger.warn(EMAIL_FAILURE_MSG + e + "\n");
 	}
-
-	//	TODO-11 (Optional): Annotate this class as a Spring-managed bean.
-	//	- Note that we enabled component scanning in an earlier step.
 
 }
